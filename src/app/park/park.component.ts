e2bd5ba3133b0a2954f8347240846602b712bf71
Amir Sacic <amir.sacic@playtech.com>
2019-01-26 13:41:50 +0100

    First Polygon in Hernals

--- src/app/park/park.component.ts
+++ src/app/park/park.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit, ViewChild } from '@angular/core';
+import { LatLngLiteral } from '@agm/core';
 
 @Component({
   selector: 'app-park',
@@ -6,10 +7,17 @@ import { Component, OnInit, ViewChild } from '@angular/core';
   styleUrls: ['./park.component.css']
 })
 export class ParkComponent implements OnInit {
-  lat: number = 51.678418;
-  lng: number = 7.809007;
+  lat: number = 48.22343080198198;
+  lng: number = 16.302846582075926;
+  zoom: number = 20;
+
+  paths: Array<LatLngLiteral> = [
+    { lng: 16.302846582075926, lat: 48.22343080198198 },
+    { lng: 16.30271056091264, lat: 48.22355413748643 },
+    { lng: 16.301884570597938, lat: 48.22341534344696 }
+  ]
   constructor() { }
   ngOnInit() {
-      
+
   }
 }
