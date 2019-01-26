4fa5f8aaa5cb9fbf2dd308713a47bbe02c4af020
Czilinger Thomas <u5EP7WW@cn.local>
2019-01-26 14:30:43 +0100

    get current location on Google Maps implemented

--- src/app/park/park.component.ts
+++ src/app/park/park.component.ts
@@ -18,6 +18,13 @@ export class ParkComponent implements OnInit {
   ]
   constructor() { }
   ngOnInit() {
-
+    if (navigator.geolocation) {
+      navigator.geolocation.getCurrentPosition((position) => {
+        this.lat=position.coords.latitude;
+        this.lng=position.coords.longitude;
+      });
+    } else {
+      alert("Geolocation is not supported by this browser.");
+    }
   }
 }
